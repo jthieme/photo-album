65db9de21a1d0814b5246152e041cc9abc87fee1
Joshua Thieme <jomthieme@gmail.com>
2023-05-30 18:31:49 -0600

    enabled fav with double click and message when search doesn't match anything

--- photo-album/src/components/Home/index.js
+++ photo-album/src/components/Home/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Card from "../Card";
 import data from "../../data/data";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 const StyledSearch = styled.div`
   width: 30%;
@@ -15,14 +17,40 @@ const StyledSearch = styled.div`
   }
 `;
 
+const StyledButton = styled.button`
+  margin-left: 2%;
+  display: flex;
+  margin-top: 3.5%;
+`;
+
+const StyledSaveButton = styled.button`
+  margin-left: 2%;
+  display: flex;
+  margin-top: 3.5%;
+`;
+
 const Home = () => {
   const [searchString, setSearchString] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const filteredAlbums = data.data.albums.filter((card) => {
+    const lowerTag = card.tag.toLowerCase();
+    const lowerSearch = searchString.toLowerCase();
+    // if the search is empty
+    if (searchString === "") {
+      return true;
+    // if the tag matches the search
+    } else if (lowerTag.includes(lowerSearch)) {
+      return true;
+    }
+    return false;
+  });
 
   return (
     <>
       <div style={{ backgroundColor: "#e1e1e1" }}>
-        <StyledSearch class="row level">
-          <div class="col-xs-9 level-item">
+        <StyledSearch className="row level">
+          <div className="col-xs-9 level-item">
             <input
               type="search"
               placeholder="Search by #"
@@ -30,20 +58,42 @@ const Home = () => {
               onChange={(e) => setSearchString(e.target.value)}
             />
           </div>
+          <input
+            type="file"
+            accept="image/*"
+            id="select-img"
+            style={{ display: "none" }}
+            onChange={(e) => {
+              setSelectedImage(e.target.files[0]);
+              console.log("selectedImage: ", selectedImage);
+            }}
+          />
+          <StyledButton
+            htmlFor="select-img"
+            onClick={() => document.getElementById("select-img")?.click()}
+          >
+            <FontAwesomeIcon
+              icon={faUpload}
+              style={{ paddingRight: "10%", marginTop: "16%" }}
+            />
+            Upload
+          </StyledButton>
+          {/* <StyledSaveButton>
+            <FontAwesomeIcon
+                icon={faUpload}
+                style={{ paddingRight: "10%", marginTop: "16%" }}
+              />
+            Save
+          </StyledSaveButton> */}
         </StyledSearch>
-        {data.data.albums
-          .filter((card) => {
-            if (searchString === "") {
-              return card;
-            } else if (
-              card.tag.toLowerCase().includes(searchString.toLowerCase())
-            ) {
-              return card;
-            }
-          })
-          .map((pics) => {
-            return <Card pics={pics} key={pics.id} />
-          })}
+
+        {filteredAlbums.length > 0 ? (
+          filteredAlbums.map((pics) => (
+            <Card pics={pics} key={pics.id} />
+          ))
+        ) : (
+          <div style={{textAlign: "center"}}>No tags match "{searchString}"</div>
+        )}
       </div>
     </>
   );
