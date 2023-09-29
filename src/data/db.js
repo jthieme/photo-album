dd7fb6d1542982975d4b80cacd0b4fa3d7080933
Joshua Thieme <jomthieme@gmail.com>
2023-09-28 22:20:15 -0600

    added a file to interact with the backend while being abstracted

--- src/data/db.js
+++ src/data/db.js
@@ -3,27 +3,50 @@ import axios from "axios";
 export const getAlbum = async (endpoint) => {
   try {
     const response = await axios.get(endpoint);
-    console.log(response.data); // You should access the data property of the response
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
-    throw error; // Re-throw the error to handle it in the caller function if needed
+    throw error;
   }
 };
 
-
 export const addCard = async (endpoint, data) => {
-    const headers = {
-        "Content-Type": "application/json",
-    };
-    try {
-        const response = await axios.post(endpoint, data, {headers});
-        console.log(response.data); // You should access the data property of the response
-        return response.data;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  try {
+    const response = await axios.post(endpoint, data, { headers });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-    } catch (error) {
-        console.error(error);
-        throw error; // Re-throw the error to handle it in the caller function if needed
-    }
+export const deleteCard = async (endpoint, data) => {
+  try {
+    // debugger;
+    // const response = await axios.delete(endpoint);
     
-}
+    // console.log(response.data);
+    // return response.data;
+    console.log("Sending DELETE request to:", endpoint);
+    console.log("data to be sent:", data)
+    const response  = await fetch(endpoint, {method: 'DELETE', body: data});
+    // const response = await fetch(endpoint, {body: data});
+    return response;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  }
+};
